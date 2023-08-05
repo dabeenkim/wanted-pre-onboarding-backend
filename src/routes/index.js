f3eaec69849d68d263bbf64c85d20b2719d3456a
dabeenkim <ekqls6812@naver.com>
2023-08-05 11:21:40 +0900

    [feat]post파일추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const authRouter = require("./auth.routes");
+const postRouter = require("./post.routes");
 
 router.use("/auth", authRouter);
+// router.use("/post", postRouter);
 
 module.exports = router;
